cc7c8a7780cab0ec3298229314e536448466b138
COBRA <COBRA@172.20.39.138>
2018-03-21 14:49:45 +0100

    Ultimo laboratorio

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -5,16 +5,25 @@
 package it.polito.tdp.spellchecker.controller;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class SpellCheckerController {
+	
+	Dictionary model = new Dictionary();
+	
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -23,7 +32,7 @@ public class SpellCheckerController {
     private URL location;
 
     @FXML // fx:id="choiceBox"
-    private ChoiceBox<?> choiceBox; // Value injected by FXMLLoader
+    private ComboBox<String> choiceBox; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtTesto"
     private TextField txtTesto; // Value injected by FXMLLoader
@@ -42,27 +51,64 @@ public class SpellCheckerController {
 
     @FXML // fx:id="labTime"
     private Label labTime; // Value injected by FXMLLoader
+    
+   // choiceBox.add("English")
+    //choiceBox.add("Italian");                 aggiungere stringhe al choice box
 
     @FXML
     void doClearText(ActionEvent event) {
+    	
+    	labTime.setText("");
+    	labError.setText("");
+    	txtError.clear();
+    	txtTesto.clear();
+    	
 
     }
 
     @FXML
     void doSpellCheck(ActionEvent event) {
+    	
+    	labTime.setText("");
+    	labError.setText("");
+    	
+    	int errori = 0;
+    	
+    	String linguaggio = choiceBox.getValue();
+    	LinkedList<String> testoTemp = new LinkedList<String>();
+    	
+    	String testo = txtTesto.getText();
+    	testo.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]\\\"", "");
+    	
+    	for(String c : testo.split(" ")) {
+    		testoTemp.add(c.toLowerCase());
+    	}
+    	
+    	model.loadDictionary(linguaggio);
+    	
+    	for(RichWord a : model.spellCheckText(testoTemp) ) {
+    		if(a.isCorretto() == false) {
+    			txtError.appendText(a.getParola()+"\n");
+    			errori++;
+    		}
+    	}
 
+    	String z = "Tempo di esecuzione"+System.nanoTime();
+    	labTime.setText(z);
+    	labError.setText("Sono stati riscontrati"+errori+" errori");
+        txtTesto.clear();
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+    	assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert btnSpeelCheck != null : "fx:id=\"btnSpeelCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert labError != null : "fx:id=\"labError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
         assert labTime != null : "fx:id=\"labTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
-
+        choiceBox.setItems(FXCollections.observableArrayList("English","Italian"));
     }
 }
 
---
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -0,0 +1,89 @@
+package it.polito.tdp.spellchecker.model;
+
+import java.io.*;
+import java.util.*;
+
+public class Dictionary {
+	
+	//LinkedList<String> dizionarioInglese = new LinkedList<String>();
+	//LinkedList<String> dizionarioItaliano = new LinkedList<String>();
+	LinkedList<String> dizionario = new LinkedList<String>();
+	
+	public Dictionary() {
+		
+	}
+
+	public void loadDictionary(String language) {
+		
+		//if(language.compareTo("English") == 0) {
+		
+		dizionario.clear();
+		
+		try { 
+			
+		FileReader fr = new FileReader("rsc/"+language+".txt");
+		BufferedReader br = new BufferedReader(fr);   
+		String word;  
+		
+		while ((word = br.readLine()) != null) { 
+			dizionario.add(word);
+		}
+			 br.close();       
+	    } catch (IOException e){   
+				 System. out.println("Errore nella lettura del file"); 
+	    }   
+		//}
+		
+		/*if(language.compareTo("Italian") == 0) {
+		
+		try { 
+			
+		FileReader fr = new FileReader("rsc/Italian.txt");
+		BufferedReader br = new BufferedReader(fr);   
+		String word;  
+		
+		while ((word = br.readLine()) != null) { 
+			dizionarioItaliano.add(word);
+		}
+			 br.close();       
+	    } catch (IOException e){   
+				 System. out.println("Errore nella lettura del file"); 
+	    }   
+		}*/
+		
+	}
+
+	public List<RichWord> spellCheckText(List<String> inputTextList){ //nel controller trasformare il testo di input in una lista di parole
+		
+		LinkedList<RichWord> richWordTemp = new LinkedList<RichWord>();
+		
+		for(String a : inputTextList) {
+			for(String e : dizionario) {
+				if(a.compareTo(e) == 0) {
+					RichWord rTemp = new RichWord(a,true);
+					richWordTemp.add(rTemp);
+				}else {
+					RichWord rTemp = new RichWord(a,false);
+					richWordTemp.add(rTemp);
+				}
+			
+			}
+		}
+		
+		/*for(String a : inputTextList) {
+			for(String e : dizionarioItaliano) {
+				if(a.compareTo(e) == 0) {
+					RichWord rTemp = new RichWord(a,true);
+					richWordTemp.add(rTemp);
+				}else {
+					RichWord rTemp = new RichWord(a,false);
+					richWordTemp.add(rTemp);
+				}
+			
+			}
+		}*/
+		
+		
+		return richWordTemp;
+	}
+}
