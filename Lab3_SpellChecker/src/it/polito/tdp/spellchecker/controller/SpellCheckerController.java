f7c0aa6a5094e9b3172eb93de6ab9a946f95d4ff
COBRA <COBRA@172.20.39.138>
2018-03-21 13:07:09 +0100

    prima modifica

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,68 @@
+/**
+ * Sample Skeleton for 'SpellChecker.fxml' Controller Class
+ */
+
 package it.polito.tdp.spellchecker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class SpellCheckerController {
-	
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="choiceBox"
+    private ChoiceBox<?> choiceBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTesto"
+    private TextField txtTesto; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnSpeelCheck"
+    private Button btnSpeelCheck; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtError"
+    private TextArea txtError; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labError"
+    private Label labError; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnClearText"
+    private Button btnClearText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labTime"
+    private Label labTime; // Value injected by FXMLLoader
+
+    @FXML
+    void doClearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnSpeelCheck != null : "fx:id=\"btnSpeelCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert labError != null : "fx:id=\"labError\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClearText != null : "fx:id=\"btnClearText\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert labTime != null : "fx:id=\"labTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+
+    }
 }
+
